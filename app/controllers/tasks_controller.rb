607ebb4d7bf2bc52fa487090fda467798565af1a
emilia grama <emiliagrama@gmail.com>
2023-11-27 11:15:48 +0100

    task manager updated

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,9 +28,15 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-private
-def task_params
-  params.require(:task).permit(:title, :details, :completed)
-end
+  def destroy
+    @task =Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
 
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,20 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+
+  #resources :tasks, only: [:create, :index, :destroy]
   get "up" => "rails/health#show", as: :rails_health_check
+
+  get "/tasks", to:"tasks#index"
+  get "/tasks/:id", to:"tasks#show", as: "task"
 
   get "tasks/new", to: "tasks#new"
   post "tasks", to: "tasks#create"
 
   get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
   patch "tasks/:id", to: "tasks#update"
-  # Defines the root path route ("/")
-  # root "posts#index"
-  get "/tasks", to:"tasks#index"
-  get "/tasks/:id", to:"tasks#show", as: "task"
+  delete "tasks/:id", to: "tasks#destroy"
 end
